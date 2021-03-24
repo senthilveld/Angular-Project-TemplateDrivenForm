2b5b04bb7ba58370b24c32430129968320db0628
senthilveld <senthilvel838@gmail.com>
2021-03-24 09:13:26 +0530

    templateForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forms';
+  itHasError = true;
+  submitted = false;
+  topics = ['Angular','React','Vue'];
+  userModel = new User('','rob@test.com',55555,'default','morning',true);
+  errorMessage ='';
+
+  constructor(private _enrollmentService:EnrollmentService){
+
+  }
+
+  validate(val:any){
+    if(val === 'default'){
+       this.itHasError = true;
+    }
+    else{
+      this.itHasError = false;
+    }
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel).subscribe(data=>console.log(data ,"success"),
+    error=>this.errorMessage = error.statusText
+    // error=>console.log(error,error.statusText ,"errorcheck")
+    )
+  }
+
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnrollmentService } from './enrollment.service';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EnrollmentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
